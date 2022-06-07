3b5812b70aed0618af5c4db73751cc67f78fcd66
Chadjaa Sofianne <67962574+chadjaa-sofiane@users.noreply.github.com>
2022-06-07 19:05:30 +0100

    setup the image

--- script.js
+++ script.js
@@ -1,9 +1,39 @@
+import imageSrc from "./imageSrc.js";
 const canavs = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
+const image = new Image();
+image.src = imageSrc;
 canavs.width = window.innerWidth;
 canavs.height = window.innerHeight;
 
-ctx.fillStyle = '#FFF';
-ctx.fillRect(100, 100, 500, 500);
-// ctx.fill()
+const drawImage = () => {
+    ctx.drawImage(image, canavs.width / 2 - 256, canavs.height / 2 - 256, 512, 512);
+}
+
+image.addEventListener("load", drawImage)
+
+// not my code, thank you copilot :>
+const throtling = (func, wait) => {
+    let timeout;
+    return function () {
+        const context = this;
+        const args = arguments;
+        const later = () => {
+            timeout = null;
+            func.apply(context, args);
+        };
+        clearTimeout(timeout);
+        timeout = setTimeout(later, wait);
+    };
+}
+
+
+// update the size of the canavs as the window size changes (throttling)
+const updateCanvasSize = throtling(() => {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    drawImage();
+}, 1000);
+
+window.addEventListener("resize", updateCanvasSize)
